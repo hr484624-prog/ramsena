55b7028e4ed75ae8bf1326262031b6ef7d62ede4
hr484624-prog <hr484624@gmail.com>
2026-08-31 17:42:40 +0530

    Create MainActivity.java

---
+++ android/app/src/main/android/app/src/main/java/com/gyansetu/app/MainActivity.java
@@ -0,0 +1,37 @@
+package com.gyansetu.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://hr484624-prog.github.io/ramsena/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+    }
